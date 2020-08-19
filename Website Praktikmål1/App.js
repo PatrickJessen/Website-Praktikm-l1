2abcc20b285cc6431d175dbe39b673e554749ef6
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2020-08-19 15:56:01 +0200

    lavet en del forside

---
+++ Website Praktikmål1/App.js
@@ -0,0 +1,21 @@
+﻿var hover = document.querySelector(".Ernæringsassistens");
+var showImage = document.querySelector(".HoverImage");
+var hover2 = document.querySelector(".Gastronom");
+var showImage2 = document.querySelector(".HoverImage2");
+
+hover.addEventListener('mouseover', function () {
+    showImage.style.visibility = 'visible';
+})
+
+hover.addEventListener('mouseout', function () {
+    showImage.style.visibility = 'hidden';
+})
+
+hover2.addEventListener('mouseover', function () {
+    showImage2.style.visibility = 'visible';
+})
+
+hover2.addEventListener('mouseout', function () {
+    showImage2.style.visibility = 'hidden';
+})
+
