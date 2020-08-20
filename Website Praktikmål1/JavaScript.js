1c9846792c732b1d335a8a44fbc73f96f96bf411
Anonymous2620 <SImranjit.Singh@Outlook.dk>
2020-08-20 13:04:02 +0200

    Initial Commit

---
+++ Website Praktikmål1/JavaScript.js
@@ -0,0 +1,15 @@
+﻿function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+    document.getElementById("demo").innerHTML = "Du er nu igang med at flytte teksten";
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("demo").innerHTML = "Teksten blev Flyttet";
+}
