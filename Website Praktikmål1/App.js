cda66e9cae2e92dde7396515c13b5b3c89405fcc
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2020-08-25 15:55:07 +0200

    lol

--- Website Praktikmål1/App.js
+++ Website Praktikmål1/App.js
@@ -1,21 +1,49 @@
-﻿var hover = document.querySelector(".Ernæringsassistens");
-var showImage = document.querySelector(".HoverImage");
-var hover2 = document.querySelector(".Gastronom");
-var showImage2 = document.querySelector(".HoverImage2");
-
-hover.addEventListener('mouseover', function () {
-    showImage.style.visibility = 'visible';
-})
-
-hover.addEventListener('mouseout', function () {
-    showImage.style.visibility = 'hidden';
-})
-
-hover2.addEventListener('mouseover', function () {
-    showImage2.style.visibility = 'visible';
-})
-
-hover2.addEventListener('mouseout', function () {
-    showImage2.style.visibility = 'hidden';
-})
-
+﻿function Hover1() {
+    document.querySelector(".HoverImage").style.visibility = "visible";
+  
+}
+function Hover1hide() {
+    document.querySelector(".HoverImage").style.visibility = "hidden";
+}
+function Hover2() {
+    document.querySelector(".HoverImage2").style.visibility = "visible";
+  
+}
+function Hover2hide() {
+    document.querySelector(".HoverImage2").style.visibility = "hidden";
+}
+function Hover3() {
+    document.querySelector(".HoverImage3").style.visibility = "visible";
+  
+}
+function Hover3hide() {
+    document.querySelector(".HoverImage3").style.visibility = "hidden";
+}
+function Hover4() {
+    document.querySelector(".HoverImage4").style.visibility = "visible";
+  
+}
+function Hover4hide() {
+    document.querySelector(".HoverImage4").style.visibility = "hidden";
+}
+function Hover5() {
+    document.querySelector(".HoverImage5").style.visibility = "visible";
+  
+}
+function Hover5hide() {
+    document.querySelector(".HoverImage5").style.visibility = "hidden";
+}
+function Hover6() {
+    document.querySelector(".HoverImage6").style.visibility = "visible";
+  
+}
+function Hover6hide() {
+    document.querySelector(".HoverImage6").style.visibility = "hidden";
+}
+function Hover7() {
+    document.querySelector(".HoverImage7").style.visibility = "visible";
+  
+}
+function Hover7hide() {
+    document.querySelector(".HoverImage7").style.visibility = "hidden";
+}
